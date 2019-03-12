4b511c2d65c267b7910434420f322c8d8ee27d19
abdella206 <abdella206@yahoo.com>
2019-03-11 21:08:52 -0700

    created logic game

--- memory_game/main.js/main.js
+++ memory_game/main.js/main.js
@@ -36,7 +36,43 @@ cardsInPlay.push(cardTwo) */
 
 //console.log("User flipped " +  cardsInPlay);
 
-var cards = ["queen", "queen ", "king", "king"];
+var cards = [
+
+            {
+             rank : "Queen",
+
+             suit: "Hearts",
+
+          cardImage:  "images/queen-of-hearts.png"
+            },
+
+            {
+              rank : "Queen",
+
+              suit:  "Diamonds",
+
+              cardImage: "images/queen-of-diamonds.png"
+
+            },
+
+            {
+              rank :  "King",
+
+              suit:   "Hearts",
+
+              cardImage: "images/king-of-hearts.png"
+
+            },
+
+            {
+              rank :  "King",
+
+              suit:   "Diamonds",
+
+              cardImage: "images/king-of-diamonds.png"
+
+            }
+];
 
 var cardsInPlay = [];
 
@@ -51,8 +87,10 @@ var checkForMatch = function () {
 
 
 var flipcards = function (cardId) {
-      console.log("User flipped " + cards[cardId]);
-      cardsInPlay.push(cards[cardId])
+      console.log("User flipped " + cards[cardId].rank);
+      console.log(cards[cardId].cardImage);
+      console.log(cards[cardId].suit);
+      cardsInPlay.push(cards[cardId].rank)
 
         if (cardsInPlay.length == 2) {
 
